4f16eed011a50d758c52b2c82bcb79ea22a623b2
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-07-10 16:23:46 +0200

    added function explanation

--- src/Algorithm/BasicArithmeticAlternativeSyntax.ts
+++ src/Algorithm/BasicArithmeticAlternativeSyntax.ts
@@ -1,3 +1,24 @@
+/*
+We'd like you to write a calculator to perform basic arithmetic, but using an alternative syntax for writing the expressions.
+
+In this alternative notation, the operators precede the operands. For example while in traditional notation we might write , instead we would write .
+
+The main advantage of this format is that it does not require parentheses for any ambiguous expression.
+
+Traditional notation	Alternative notation
+3 + 4	+ 3 4
+3 - (4 * 5)	- 3 * 4 5
+(3 + 4) * 5	* + 3 4 5
+(3 - 4) / (5 + 2)	/ - 3 4 + 5 2
+In the code provided, the  file exports a  function. This function is expected to take an alternative expression as a string, and output the numerical solution.
+
+We have included some tests to check this function works as expected.
+
+To run the code:
+
+Please implement the calculate function to solve expressions in the alternative format as expected.
+*/
+
 export function expression(expression: string): number {
     const tokens: string[] = expression.split(' ');
 
